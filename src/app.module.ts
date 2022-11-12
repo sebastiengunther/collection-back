f7d9a3a49262f0440c0443e688013a777014c331
Sébastien <sgunther@laposte.net>
2022-11-12 16:57:27 +0100

    Add CommonModule and CollectionModule in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CollectionModule } from './collection/collection.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [CommonModule, CollectionModule],
   controllers: [AppController],
   providers: [AppService],
 })
