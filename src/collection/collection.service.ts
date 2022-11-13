bb350f7dcf29fa1b3c6a5cc0991d07481a69b7fa
Sébastien <sgunther@laposte.net>
2022-11-13 14:44:35 +0100

    Add S3 Bucket file upload
    S3 Bucket files are public, to get them and potentially display them on a webpage
    
    AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_BUCKET_NAME are set in the .env file

--- src/collection/collection.service.ts
+++ src/collection/collection.service.ts
@@ -3,18 +3,37 @@ import { CreateCollectionInput } from './dto/create-collection.input';
 import { Collection } from './models/collection.model';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { S3 } from 'aws-sdk';
+import { ConfigService } from '@nestjs/config';
+import { dataUriToBuffer } from 'src/utils/DataUri';
 
 @Injectable()
 export class CollectionService {
+  private readonly s3: S3;
+
   constructor(
     @InjectModel(Collection.name)
     private readonly collectionModel: Model<Collection>,
-  ) {}
+  ) {
+    this.s3 = new S3({
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    });
+  }
 
-  create(createCollectionInput: CreateCollectionInput) {
+  async create(createCollectionInput: CreateCollectionInput) {
     const collection = new this.collectionModel(createCollectionInput);
+
     // Todo : Verify fields
-    // Todo : Save picture in S3
+    
+    const fileType = collection.file.split(',')[0].match(/\/(.+?)[\+;]/)?.[1];
+    const uploadedImage = await this.s3.upload({
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: `${Date.now().toString()}.${fileType}`,
+      Body: dataUriToBuffer(collection.file),
+    }).promise();
+    collection.file = uploadedImage.Location;
+
     collection.date = new Date();
     return collection.save();
   }
