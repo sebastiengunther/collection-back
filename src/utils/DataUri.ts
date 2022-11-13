440729d8358267406ee24ed31789c5b69e29f2d4
Sébastien <sgunther@laposte.net>
2022-11-13 16:44:26 +0100

    Change dataUriToFile by dataUriSize and dataUriType
    File does not exists here, we need to get the type and the size

--- src/utils/DataUri.ts
+++ src/utils/DataUri.ts
@@ -1,25 +1,38 @@
 import { Buffer } from 'buffer';
 
-export function dataUriToFile(dataUri: string) {
-  if(!dataUri.match(/data:.+;base64,.+/)) {
-    throw new Error('Error: Parameter "dataUri" is not a Data URI. Data URI format should be "data:<mime>;base64,<b64>".');
+export function dataUriToBuffer(dataUri: string) {
+  if (!dataUri.match(/data:.+;base64,.+/)) {
+    throw new Error(
+      'Parameter "dataUri" is not a Data URI. Data URI format should be "data:<mime>;base64,<b64>".',
+    );
   }
-  
-  const array = dataUri.split(',');
-  const type = array[0].match(/:(.+?);/)?.[1];
-  const buffer = Buffer.from(array[1], 'base64');
-  const arrayBuffer = buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
 
-  return new File([arrayBuffer], Date.now().toString(), {type: type});
+  const array = dataUri.split(',');
+  const buffer = Buffer.from(array[1], 'base64');
+
+  return buffer;
 }
 
-export function dataUriToBuffer(dataUri: string) {
-  if(!dataUri.match(/data:.+;base64,.+/)) {
-    throw new Error('Error: Parameter "dataUri" is not a Data URI. Data URI format should be "data:<mime>;base64,<b64>".');
+export function dataUriSize(dataUri: string) {
+  if (!dataUri.match(/data:.+;base64,.+/)) {
+    throw new Error(
+      'Parameter "dataUri" is not a Data URI. Data URI format should be "data:<mime>;base64,<b64>".',
+    );
   }
-  
+
   const array = dataUri.split(',');
-  const buffer = Buffer.from(array[1], 'base64');
-    
-  return buffer;
+
+  return (array[1].length * 6) / 8;
+}
+
+export function dataUriType(dataUri: string) {
+  if (!dataUri.match(/data:.+;base64,.+/)) {
+    throw new Error(
+      'Parameter "dataUri" is not a Data URI. Data URI format should be "data:<mime>;base64,<b64>".',
+    );
+  }
+
+  const array = dataUri.split(',');
+
+  return array[0].match(/:(.+?);/)?.[1];
 }
